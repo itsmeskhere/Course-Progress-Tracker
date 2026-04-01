a7c43163269138db73e4f3652e961d26aa0cd964
Suman Kalyan Giri (SK) <sumankalyangiri786@gmail.com>
2026-04-01 23:03:38 +0530

    Add Jonas Node JS course and remove C++ courses

--- data.js
+++ data.js
@@ -15,32 +15,36 @@ const courses = {
     name: "Jonas React JS",
     dataUrl: "courses_data/jonas-react-js.json",
   },
+  "jonas-node-js": {
+    name: "Jonas Node JS",
+    dataUrl: "courses_data/jonas-node-js.json",
+  },
   "brad-html-css": {
     name: "Brad Traversy HTML CSS",
     dataUrl: "courses_data/brad-html-css.json",
   },
-  "cpp-nanodegree-welcome": {
-    name: "C++ Nanodegree: Welcome",
-    dataUrl: "courses_data/cpp-nanodegree-welcome.json",
-  },
-  "cpp-nanodegree-cpp-foundations": {
-    name: "C++ Nanodegree: C++ Foundations",
-    dataUrl: "courses_data/cpp-nanodegree-cpp-foundations.json",
-  },
-  "cpp-nanodegree-object-oriented-programming": {
-    name: "C++ Nanodegree: Object-Oriented Programming",
-    dataUrl: "courses_data/cpp-nanodegree-object-oriented-programming.json",
-  },
-  "cpp-nanodegree-memory-management": {
-    name: "C++ Nanodegree: Memory Management",
-    dataUrl: "courses_data/cpp-nanodegree-memory-management.json",
-  },
-  "cpp-nanodegree-concurrency": {
-    name: "C++ Nanodegree: Concurrency",
-    dataUrl: "courses_data/cpp-nanodegree-concurrency.json",
-  },
-  "cpp-nanodegree-capstone": {
-    name: "C++ Nanodegree: Capstone",
-    dataUrl: "courses_data/cpp-nanodegree-capstone.json",
-  },
+  // "cpp-nanodegree-welcome": {
+  //   name: "C++ Nanodegree: Welcome",
+  //   dataUrl: "courses_data/cpp-nanodegree-welcome.json",
+  // },
+  // "cpp-nanodegree-cpp-foundations": {
+  //   name: "C++ Nanodegree: C++ Foundations",
+  //   dataUrl: "courses_data/cpp-nanodegree-cpp-foundations.json",
+  // },
+  // "cpp-nanodegree-object-oriented-programming": {
+  //   name: "C++ Nanodegree: Object-Oriented Programming",
+  //   dataUrl: "courses_data/cpp-nanodegree-object-oriented-programming.json",
+  // },
+  // "cpp-nanodegree-memory-management": {
+  //   name: "C++ Nanodegree: Memory Management",
+  //   dataUrl: "courses_data/cpp-nanodegree-memory-management.json",
+  // },
+  // "cpp-nanodegree-concurrency": {
+  //   name: "C++ Nanodegree: Concurrency",
+  //   dataUrl: "courses_data/cpp-nanodegree-concurrency.json",
+  // },
+  // "cpp-nanodegree-capstone": {
+  //   name: "C++ Nanodegree: Capstone",
+  //   dataUrl: "courses_data/cpp-nanodegree-capstone.json",
+  // },
 };
